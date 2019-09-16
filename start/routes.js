ac271600082d3d1a06fa9b1adf2e35a342830c36
KevinR360 <kevin.rs.201057@gmail.com>
2019-09-16 14:10:20 -0300

    learning:Adonis-CRUD-Projects

--- start/routes.js
+++ start/routes.js
@@ -22,5 +22,10 @@ Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
 
-Route.post('/files', 'FileController.store')
 Route.get('/files/:id', 'FileController.show')
+
+Route.group(() => {
+  Route.post('/files', 'FileController.store')
+
+  Route.resource('projects', 'ProjectController').apiOnly()
+}).middleware(['auth'])
